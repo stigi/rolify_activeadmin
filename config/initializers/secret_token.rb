c35b92f40caece23c5592bd9c5baa92dc9279ce4
Ullrich Schäfer <ullrich@seidbereit.de>
2012-04-13 22:19:12 +0200

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RolifyActiveadmin::Application.config.secret_token = '82b3c21f6a9861d8c79327a8689a64e98db649d7a095a4ea5d207e04e9d47e46610f3ff020036bb4fc86ab1f1fd5d09db16c643dd30b76daf7fb576ba17bcf8a'
