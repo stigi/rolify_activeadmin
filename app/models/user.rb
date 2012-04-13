a318ca60989093e0fc8d2f6a258c48ea323e0db2
Ullrich Schäfer <ullrich@seidbereit.de>
2012-04-13 22:59:24 +0200

    rails g rolify:role role user mongoid

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User
+	rolify:role_cname => 'Role'
   include Mongoid::Document
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
