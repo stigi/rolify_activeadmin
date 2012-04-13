426ec4cb4c5841292739ec31cef63ca6e41ac2ea
Ullrich Schäfer <ullrich@seidbereit.de>
2012-04-13 23:07:35 +0200

    work around for rolify issue 57
    - also `extend Rolify` needs to be after `include Mongoid::Document`

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User
-	rolify:role_cname => 'Role'
   include Mongoid::Document
+  extend Rolify
+  rolify:role_cname => 'Role'
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
